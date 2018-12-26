ac7df55c8dd6da51d02cb9e9e7eb83854a8de139
Alex Skrypnyk <alex.designworks@gmail.com>
2018-12-27 10:19:52 +1100

    Fixed AV install.

--- .docker/Dockerfile.php
+++ .docker/Dockerfile.php
@@ -2,11 +2,11 @@
 ARG CLI_IMAGE
 FROM ${CLI_IMAGE:-cli} as cli
 
-RUN apk --update add clamav clamav-libunrar \
-    # Antivirus update returns non-zero codes.
-    # @see https://github.com/clamwin/clamav/blob/0.100.1/freshclam/freshclamcodes.h#L23
-    && freshclam || freshclam_exit=$? && [ "$freshclam_exit" == "1" ]
-
 FROM amazeeio/php:7.1-fpm
+
+# Antivirus update returns non-zero codes.
+# @see https://github.com/clamwin/clamav/blob/0.100.1/freshclam/freshclamcodes.h#L23
+RUN apk --update add clamav clamav-libunrar \
+    && freshclam --no-warnings || true
 
 COPY --from=cli /app /app
