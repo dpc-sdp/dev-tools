f421201a2018f690860c1b2b73b99214332798fc
Alex Skrypnyk <alex.designworks@gmail.com>
2018-12-27 00:12:28 +1100

    Fixed clamav.

--- .docker/Dockerfile.php
+++ .docker/Dockerfile.php
@@ -3,7 +3,9 @@ ARG CLI_IMAGE
 FROM ${CLI_IMAGE:-cli} as cli
 
 RUN apk --update add clamav clamav-libunrar \
-    && freshclam
+    # Antivirus update returns non-zero codes.
+    # @see https://github.com/clamwin/clamav/blob/0.100.1/freshclam/freshclamcodes.h#L23
+    && freshclam || freshclam_exit=$? && [ "$freshclam_exit" == "1" ]
 
 FROM amazeeio/php:7.1-fpm
 
