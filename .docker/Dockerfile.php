9c77ae817c04e36b5460f2a92b9be1c7d9ca6b4a
markxtji <markxtji@gmail.com>
2019-04-02 12:36:43 +1100

    removed steps that have been implemented in parent images (#11)

--- .docker/Dockerfile.php
+++ .docker/Dockerfile.php
@@ -6,7 +6,6 @@ FROM singledigital/bay-php:latest
 
 # Antivirus update returns non-zero codes.
 # @see https://github.com/clamwin/clamav/blob/0.100.1/freshclam/freshclamcodes.h#L23
-RUN apk --update add clamav clamav-libunrar \
-    && freshclam --no-warnings || true
+RUN  freshclam --no-warnings || true
 
 COPY --from=cli /app /app
