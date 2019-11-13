96ae41bcdda3becfda4552d858025dc69799ff67
Guy Owen <guy.owen@dpc.vic.gov.au>
2019-11-13 13:03:38 +1100

    Added tide_grant repo. Specified Dev-Tools commit.

--- dev-tools.sh
+++ dev-tools.sh
@@ -33,6 +33,6 @@
 # stable version.
 #
 # Uncomment and set the Dev-Tools's commit value and commit this change.
-# export GH_COMMIT=COMMIT_SHA
+export GH_COMMIT=7c3669b2ddad2008ab29ed1fe3a247160d578083
 
 bash <(curl -L https://raw.githubusercontent.com/dpc-sdp/dev-tools/master/install?"$(date +%s)") "$@"
