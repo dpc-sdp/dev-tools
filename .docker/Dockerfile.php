9c8ffa1b0e5684a98e1a24ae682864e902227283
Anthony Malkoun <anthony.malkoun@dpc.vic.gov.au>
2019-11-15 11:45:49 +1100

    Remove ClamAV from PHP image

--- .docker/Dockerfile.php
+++ .docker/Dockerfile.php
@@ -6,9 +6,4 @@ FROM ${CLI_IMAGE:-cli} as cli
 
 FROM singledigital/bay-php:${BAY_IMAGE_VERSION}
 
-# Antivirus update returns non-zero codes.
-# @see https://github.com/clamwin/clamav/blob/0.100.1/freshclam/freshclamcodes.h#L23
-RUN apk --update add clamav clamav-libunrar \
-&& freshclam --no-warnings || true
-
 COPY --from=cli /app /app
