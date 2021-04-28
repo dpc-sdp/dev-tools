8d73b8a8a92856b78f7f66701e76e5e2e7be580d
Vincent Gao <vincent.gao@dpc.vic.gov.au>
2021-04-28 11:09:21 +1000

    test

--- .circleci/test.sh
+++ .circleci/test.sh
@@ -12,5 +12,4 @@ mkdir -p /tmp/artifacts/behat
 ahoy test-behat || ahoy test-behat -- --rerun
 
 echo "==> Run Unit tests"
-mkdir -p /tmp/unit_test
-../vendor/bin/phpunit
+ahoy cli "/app/vendor/bin/phpunit"
