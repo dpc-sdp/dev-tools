5f9dfe2f888fabce6449c9f42b4e31401698b5a5
Ming Quah <ming.quah@protonmail.com>
2021-12-02 15:54:33 +1100

    [SDPA-5674] Update phpdotenv to 4.1

--- scripts/composer/DrupalSettings.php
+++ scripts/composer/DrupalSettings.php
@@ -141,7 +141,7 @@ FILE;
   protected static function extractEnvironmentVariables(array $allowed) {
     $options = [];
 
-    $dotenv = new Dotenv(__DIR__ . '/../..');
+    $dotenv = Dotenv::createMutable(__DIR__ . '/../..');
     $dotenv->load();
 
     foreach ($allowed as $name) {
